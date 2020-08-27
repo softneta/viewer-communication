61e7baee77213511855d0bfe6b624f130ec15e9b
Karolis Pleiris <karolis.pleiris@softneta.com>
2020-08-27 17:00:09 +0300

    feat: Added Window open functions.
    refs: #46998

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 (function(globals) {
     'use strict';
 
-    function ViewerCommunication (url) {
-        const targetURL = url;
+    function ViewerCommunication (targetURL, integration = 'study') {
+        let windowReference;
+        const integrationType = integration === 'study' ? 'study' : 'token';
         const functions = {};
 
-        functions.test = function () {
-            console.log(targetURL);
+        functions.getWindowReference = function () {
+            if (!windowReference || windowReference && windowReference.closed) {
+                windowReference = window.open('', 'MedDreamViewer');
+            }
+            return windowReference;
         };
+
+        functions.postActionMessage = function (actionType, actionData) {
+            const reference = this.getWindowReference();
+            if (reference) {
+                reference.postMessage({actionType, actionData}, targetURL);
+            }
+        };
+
+        functions.openInMedDream = function (value) {
+            windowReference = window.open(`${targetURL}?${integrationType}=${value}`, "_blank");
+        };
+
+        functions.addToMedDream = function (value) {
+            windowReference = window.open(`${targetURL}?${integrationType}=${value}&add=true`, "_blank");
+        };
+
+        functions.replaceInMedDream = function (value) {
+            windowReference = window.open(`${targetURL}?${integrationType}=${value}&replace=true`, "_blank");
+        };
+
+        /*if (integration === 'study') {
+
+        } else {
+
+        }*/
 
         return functions;
     }
